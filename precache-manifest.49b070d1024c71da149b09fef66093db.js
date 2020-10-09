1943bf168efee0b609efd6605f6c333e6398e07d
Javier Cabrera <cabrera.javier@gmail.com>
2020-10-09 15:56:49 +0200

    Updates

---
+++ precache-manifest.49b070d1024c71da149b09fef66093db.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a32b36892238f9bbd447d351840a2b",
+    "url": "/jcc2303-react/index.html"
+  },
+  {
+    "revision": "759528e5ded4c756bf4f",
+    "url": "/jcc2303-react/static/css/main.615521c1.chunk.css"
+  },
+  {
+    "revision": "b639fa5bd4ad62022111",
+    "url": "/jcc2303-react/static/js/2.8d116e22.chunk.js"
+  },
+  {
+    "revision": "b6a57e0678f5f23c667a8321b1b88197",
+    "url": "/jcc2303-react/static/js/2.8d116e22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "759528e5ded4c756bf4f",
+    "url": "/jcc2303-react/static/js/main.5f18fd81.chunk.js"
+  },
+  {
+    "revision": "4202dabb7f34f0a2bce8",
+    "url": "/jcc2303-react/static/js/runtime-main.1fb2cd87.js"
+  },
+  {
+    "revision": "6c46e584bfebf26d1960a72d5be08b77",
+    "url": "/jcc2303-react/static/media/arrow-alt-circle-down.6c46e584.svg"
+  },
+  {
+    "revision": "5e9fe16918edf79e37d3cd7afea9b9f8",
+    "url": "/jcc2303-react/static/media/bars.5e9fe169.svg"
+  },
+  {
+    "revision": "b1f2a76280a27ad7ff98924882c41e78",
+    "url": "/jcc2303-react/static/media/close.b1f2a762.svg"
+  },
+  {
+    "revision": "01414b2f5e53eba7bd703c3ad8a3902e",
+    "url": "/jcc2303-react/static/media/hearth.01414b2f.svg"
+  },
+  {
+    "revision": "1d72ad51bb70e006b868fbcf0fd4cc80",
+    "url": "/jcc2303-react/static/media/logo.1d72ad51.svg"
+  }
+]);
